77631ac6283df9c263174862c322decec430e291
trebloc <ctse77@alum.berkeley.edu>
2015-11-25 14:15:22 -0800

    update

--- game-1/main.js
+++ game-1/main.js
@@ -4,40 +4,63 @@ $(function() {
 // wait for DOM to load before running JS
 console.log("Sanity Check: JS is working!");
 
-$('.player').click(function(elem) {
-$('.player').text("x");
-});
-});
+var p1Distance = 2;
+var p2Distance = 2;
+var $player1 = $(".player").eq(0);
+var $player2 = $(".player").eq(1);
 
-/*
-// elements for gameplay
 
-var $board = $('#board'),
-    $boxes1 = $('.box'),
-    // $boxes2 = $('.box2'),    
-    $reset = $('#reset');
-
-//players
-var player1 = "x";
-// var player2 = "o";
-
-// counter for reset
-var count = 0;
-
-var $player = $(".box");
-var leftPosition = $player.offset().left;
-
-$(".boxes1").keypress(function handler() {
-  $(".box" + player1).text("");
-  player1 ++
-
-  $(".box" + player1).text("O");
+$(window).on("keypress", function handler(event) {
+  if (event.which === 97) {
+    if ($player1.css("left") !== "96%") {
+      p1Distance += 2;
+      $player1.css("left", p1Distance + "%");
+    } else if ($player1.css("left") === "96%") {
+      console.log("Winner!");
+    }
+  } else if (event.which === 108) {
+    if ($player2.css("left") !== "96%") {
+      p2Distance += 2;
+      $player2.css("left", p2Distance + "%"); 
+    } else if ($player2.css("left") === "96%") {
+      console.log("Winner O");
+    }
+  }
 });
 
-
-// Resets the Game.
-$reset.click(function clickHandle() {
-    count = 0;
-    $boxes.empty();
+//$('.player').click(function(elem) {
+// $('.player').text("Hi");
 });
-*/
+
+              /*
+              // elements for gameplay
+
+              var $board = $('#board'),
+                  $boxes1 = $('.box'),
+                  // $boxes2 = $('.box2'),    
+                  $reset = $('#reset');
+
+              //players
+              var player1 = "x";
+              // var player2 = "o";
+
+              // counter for reset
+              var count = 0;
+
+              var $player = $(".box");
+              var leftPosition = $player.offset().left;
+
+              $(".boxes1").keypress(function handler() {
+                $(".box" + player1).text("");
+                player1 ++
+
+                $(".box" + player1).text("O");
+              });
+
+
+              // Resets the Game.
+              $reset.click(function clickHandle() {
+                  count = 0;
+                  $boxes.empty();
+              });
+              */

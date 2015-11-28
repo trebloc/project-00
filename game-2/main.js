bc1dc9957e73f94aced0523fcef6ff12fac16d2c
trebloc <ctse77@alum.berkeley.edu>
2015-11-27 22:37:01 -0800

    finished the board, but need to work on JS

--- game-2/main.js
+++ game-2/main.js
@@ -3,4 +3,23 @@ $(function() {
 
 // wait for DOM to load before running JS
 console.log("Sanity Check: JS is working!");
+    
+var player1 = 0;
+var player2 = 0;
+    
+    $(window).on("keypress", function handler(event) {
+      
+       if (event.which === 97) {
+        if (player1 < 11) {
+          $(".box").eq(player1).text("");
+            player1 ++;
+            $(".box").eq(player1).text("X");
+          } else if (ply1 <= 10) {
+        }      
 
+
+        
+
+
+});
+});

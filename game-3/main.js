21cb3d07d8b306c27c47c0f9da5290c437c08b22
trebloc <ctse77@alum.berkeley.edu>
2015-11-29 15:46:49 -0800

    added 2nd player functionality

--- game-3/main.js
+++ game-3/main.js
@@ -6,12 +6,15 @@ $("iframe").hide();
     
 //Defining Player 1 and 2 Position   
 var player1 = 0;
-var player2 = 0;
+var player2 = 11;
 
 var enterprise = new Image (100, 33);
 enterprise.src = "images/enterprise.gif";
+var enterprise2 = new Image (100, 33);
+enterprise2.src = "images/enterprise.jpg";
 
 $("."+ player1).append(enterprise);
+$("."+ player2).append(enterprise2);
 
 
     
@@ -20,24 +23,22 @@ $("."+ player1).append(enterprise);
       
 //Player 1 and 2      
        if (event.which === 97) {
-        if (player1 < 11) {
-          
+        if (player1 < 10) {          
             player1 ++;
             $("."+ player1).append(enterprise);
         }             
       } else if (event.which === 108) {
-        if (player2 < 11) {
-          $(".box2").eq(player2).text("");
-          player2 ++;
-          $(".box2").eq(player2).text("O");
-        }
+        if (player2 < 21) {          
+            player2 ++;
+            $("."+ player2).append(enterprise2);
+        }   
       }    
 //Win Outcome                  
-      if (player1 >= 11) {
+      if (player1 >= 10) {
   $(".winner").text("Player One Wins!")
   $(window).off("keypress");
 }
-  else if (player2 >=11) {
+  else if (player2 >=21) {
     $(".winner").text("Player Two Wins!")
     $(window).off("keypress");    
   }

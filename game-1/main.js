a8c060c1e22bfbccc4ca8c7c8bd4b7ab9d2c4313
trebloc <ctse77@alum.berkeley.edu>
2015-11-29 11:38:01 -0800

    set up comments and finished the player 2 game play.

--- game-1/main.js
+++ game-1/main.js
@@ -3,62 +3,44 @@ $(function() {
 // wait for DOM to load before running JS
 console.log("Sanity Check: JS is working!");
 
+// Setting the Variables for distance on the board and the players.
 var p1Distance = 2;
 var p2Distance = 2;
 var $player1 = $(".player").eq(0);
 var $player2 = $(".player").eq(1);
 
+// Setup of the Gameboard Width so that PX is stripped and working on Percentages.
+var gameBoard = parseInt($(".col-md-12").css("width"), 10);
 
+// Setting the Keypress for "A" and "L" for player 1 and 2 respectively and the Game Outcomes.
 $(window).on("keypress", function handler(event) {
   if (event.which === 97) {
     //Trying to declare a winner when a player hits certain % at the end. 
     if ($player1.css("left")) {
       p1Distance += 2;
       $player1.css("left", p1Distance + "%");
+      if ( parseInt($player1.css("left"), 10) >= (0.85 * gameBoard)) {
+        $(".winner").append("<h1>Player 1 wins</h1>");
+        $(window).off("keypress");
+      }
     }
   } else if (event.which === 108) {
     if ($player2.css("left")) {
       p2Distance += 2;
       $player2.css("left", p2Distance + "%");
+      if ( parseInt($player2.css("left"), 10) >= (0.85 * gameBoard)) {
+        $(".winner").append("<h1>Player 2 wins</h1>");
+        $(window).off("keypress");
     }
   }
-
+}
 });
 
+// Game Reset
 var $bothPlayers = $(".player");
-// Resets the Game.
+//
     $("button").click(function handleClick(event){
     location.reload();
 
 }); 
-
- 
-
-//$('.player').click(function(elem) {
-// $('.player').text("Hi");
 });
-
-              /*
-              // elements for gameplay
-              var $board = $('#board'),
-                  $boxes1 = $('.box'),
-                  // $boxes2 = $('.box2'),    
-                  $reset = $('#reset');
-              //players
-              var player1 = "x";
-              // var player2 = "o";
-              // counter for reset
-              var count = 0;
-              var $player = $(".box");
-              var leftPosition = $player.offset().left;
-              $(".boxes1").keypress(function handler() {
-                $(".box" + player1).text("");
-                player1 ++
-                $(".box" + player1).text("O");
-              });
-              // Resets the Game.
-              $reset.click(function clickHandle() {
-                  count = 0;
-                  $boxes.empty();
-              });
-              */

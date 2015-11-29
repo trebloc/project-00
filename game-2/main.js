c7e1261a7e3eb40976e524b2d2de02b80f7ee3d5
trebloc <ctse77@alum.berkeley.edu>
2015-11-28 18:39:30 -0800

    fixed the visual elements with CSS for text and layout

--- game-2/main.js
+++ game-2/main.js
@@ -4,11 +4,14 @@ $(function() {
 console.log("Sanity Check: JS is working!");
 $("iframe").hide();
     
+//Defining Player 1 and 2    
 var player1 = 0;
 var player2 = 0;
     
+//Keypress set for "A" (Player 1) and "L" (Player 2)    
     $(window).on("keypress", function handler(event) {
       
+//Player 1 and 2 Movement      
        if (event.which === 97) {
         if (player1 < 11) {
           $(".box").eq(player1).text("");
@@ -21,18 +24,20 @@ var player2 = 0;
           player2 ++;
           $(".box2").eq(player2).text("O");
         }
-      }                
+      }    
+//Win Outcome                  
       if (player1 >= 11) {
-  $(".boxwinner").text("Player One Wins!")
-
+  $(".winner").text("Player One Wins!")
+  $(window).off("keypress");
 }
   else if (player2 >=11) {
-    $(".boxwinner").text("Player Two Wins!")
+    $(".winner").text("Player Two Wins!")
+    $(window).off("keypress");    
   }
     });
 
 
-
+//Reset
     $("button").click(function handleClick(event){
     location.reload();
 
